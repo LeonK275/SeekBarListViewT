39706910a7ec456b44dacffccc1b165dc596e112
Leon <mail@leonkuehn.de>
2025-02-14 16:25:19 +0100

    first commit

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.listview;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            ListView listView = findViewById(R.id.listView21);
+            final ArrayList<String> myFamily = new ArrayList<String>();
+            myFamily.add(String.valueOf(1));
+            myFamily.add("Sarazar");
+            myFamily.add("Sgt Rumpel");
+            myFamily.add("Leon");
+
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
+            listView.setAdapter(arrayAdapter);
+
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Log.i("Clicked Person", myFamily.get(position));
+                }
+            });
+
+
+            return insets;
+
+
+        });
+    }
+}
